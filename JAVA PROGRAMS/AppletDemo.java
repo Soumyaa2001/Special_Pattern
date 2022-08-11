76d16cda7c80fa70293e381249b97e8f20710c51
Soumyaneel Ghosh <90539633+Soumyaa2001@users.noreply.github.com>
2022-08-11 12:47:39 +0530

    Delete JAVA PROGRAMS directory

--- JAVA PROGRAMS/AppletDemo.java
+++
@@ -1,9 +0,0 @@
-import java.applet.Applet;
-import java.awt.Graphics;
-public class AppletDemo extends Applet
-{
-public void paint(Graphics g)
-{
-g.drawString("HELLO BE COLLAGE", 50, 50);
-}
-}
--- JAVA PROGRAMS/Welcome.java
+++
@@ -1,7 +0,0 @@
-class Welcome
-{
-public static void main(String args[])
-{
-System.out.println("WELCOME TO JAVA WORLD!!");
-}
-}
